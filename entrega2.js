26f46821ecb05097096e51a272be7a6353637ebc
Edward Espinoza <edwesti2710@gmail.com>
2022-08-17 23:39:48 -0500

    Mejorando la sintaxis

--- entrega2.js
+++ entrega2.js
@@ -4,16 +4,16 @@ const fecha = new Date();
 
 setTimeout(function () {
     let tipoRespuesta = parseInt(prompt('1 o 2'));
-    if (tipoRespuesta == 1 || tipoRespuesta == 2){
+    if (tipoRespuesta == 1 || tipoRespuesta == 2) {
         let anoNacimiento = parseInt(prompt('Ingresa tu año de nacimiento:'));
 
-        if (anoNacimiento < (fecha.getFullYear()-100)) {
+        if (anoNacimiento < (fecha.getFullYear() - 100)) {
             mostrarRespuesta(tipoRespuesta, 'Tienes más de 100 años, yo que tu estaría aprovechando el tiempo en vez de hacer esto :v')
         } else {
             let edadActual = parseInt(fecha.getFullYear()) - anoNacimiento;
             let mesNacimiento = parseInt(prompt('Ingresa tu mes de nacimiento (Número):'));
             let diaNacimiento = parseInt(prompt('Ingresa tu día de nacimiento (Número):'));
-        
+
             findPares(edadActual, mesNacimiento, diaNacimiento)
 
         }
@@ -21,31 +21,31 @@ setTimeout(function () {
         alert('¿Sabes leer?')
     }
 
-    function findPares(edadActual, mesNacimiento, diaNacimiento){
-        while (edadActual > 0){
-            if (edadActual % 2 === 0){
+    function findPares(edadActual, mesNacimiento, diaNacimiento) {
+        while (edadActual > 0) {
+            if (edadActual % 2 === 0) {
                 resultado.push(edadActual);
                 contador++;
             }
             edadActual--
         }
-    
-        if (mesNacimiento > fecha.getMonth()+1){
+
+        if (mesNacimiento > fecha.getMonth() + 1) {
             resultado.shift();
             contador--;
-        } else if (mesNacimiento == fecha.getMonth()+1){
-            if (diaNacimiento > fecha.getDate()){
+        } else if (mesNacimiento == fecha.getMonth() + 1) {
+            if (diaNacimiento > fecha.getDate()) {
                 resultado.shift();
                 contador--;
             }
         }
-        mostrarRespuesta(tipoRespuesta, 'Tuviste '+contador+' años pares:\n'+resultado);
+        mostrarRespuesta(tipoRespuesta, 'Tuviste ' + contador + ' años pares:\n' + resultado);
     }
-    
-    function mostrarRespuesta(tipo, mensaje){
-        if (tipo === 2){
+
+    function mostrarRespuesta(tipo, mensaje) {
+        if (tipo === 2) {
             console.log(mensaje);
-        } else if (tipo === 1){
+        } else if (tipo === 1) {
             alert(mensaje);
         }
     }
